41315243f5f3834615cfec61a32a62326fd71f5b
Anthony <navyseals1405@gmail.com>
2020-09-29 14:06:55 -0500

    Added Canvas and Turtles, and a while statement with some colors //TODO: Brush width changes within the while statement & creating an actual pattern with two turtles

--- TurtleLab.java
+++ TurtleLab.java
@@ -4,6 +4,22 @@ public class TurtleLab
 {
     public static void main(String[] args)
     {
-        //Due
+        // Turtles & Canvas
+        World canvas = new World();
+        Turtle paint = new Turtle(canvas);
+        Turtle brush = new Turtle(canvas);
+        // Turtle Settings + Extra
+        paint.setPenColor(Color.RED);
+        paint.setPenWidth(10);
+        brush.setPenColor(Color.BLUE);
+        int timer = 250;
+        int[] penWidth = {1,2,4,8,16,32};
+        
+        while (timer != 0)
+        {
+            paint.forward(25);
+            timer--;
+        }
+        
     }
 }
